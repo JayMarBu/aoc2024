d4c5ca50e28054c650997037203a1dae770c39ee
JayMarBu <jaymarbu@gmail.com>
2024-11-28 20:25:36 +0000

    initial commit

---
+++ premake5.lua
@@ -0,0 +1,63 @@
+workspace "AOC2024"
+    -- location
+    architecture "x64"
+    startproject "AOC"
+    configurations
+    {
+        "Debug",
+        "Release"
+    }
+
+GOL_PATH = "E:/_Dev/gol"
+
+include (GOL_PATH .. "/gollib.lua")
+
+project "AOC"
+	kind "ConsoleApp"
+	language "C++"
+	staticruntime "on"
+		
+	targetdir "build/bin/%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}/%{prj.name}"
+	objdir "build/bin-int/%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}/%{prj.name}"
+
+	filter "action:vs2022"
+		cppdialect "C++20"
+		buildoptions  {"/Zc:preprocessor"}
+	
+	filter "configurations:Debug"
+		defines "GOL_DEBUG"
+		runtime "Debug"
+		optimize "off"
+		symbols "on"
+	
+	filter "configurations:Release"
+		defines "GOL_RELEASE"
+		runtime "Release"
+		optimize "on"
+		symbols "off"
+	
+	filter {}
+	
+	defines
+	{
+		--"GOL_USE_CPP_STD"
+	}
+	
+	links
+	{
+		"Golem",
+	}
+	
+	includedirs
+	{
+		"src/",
+		(GOL_PATH.."/src/")
+	}
+	
+	files
+	{
+		"premake5.lua",
+		"src/**.cpp",
+		"src/**.h",
+		"res/**.txt",
+	}

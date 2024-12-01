caddc486cd9274bbba0a36d9d7e5a3b9c4ef6d76
Jay Marsden-Bullen <JayMarBu@gmail.com>
2024-12-01 17:21:27 +0000

    day 1 complete

--- src/AocUtils.h
+++ src/AocUtils.h
@@ -5,13 +5,15 @@
 
 #define CHAR_TO_INT(c) (c - 48)
 
+constexpr bool TEST_DATA = true;
+
 namespace AoC
 {
 	inline bool CharIsNumber(char c) { return c >= '0' && c <= '9'; }
 	inline bool CharIsAlphabet(char c) { return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'); }
 	inline bool CharIsLineEnd(char c) { return c == '\n' || c == '\r'; }
 
-	gol::File LoadData(int day, bool testData)
+	gol::File LoadData(int day, bool testData = false)
 	{
 		char buff[128];
 		gol::StrFmt(buff, 128, "res/day%i/%s", day, testData ? "test_data.txt" : "data.txt");
--- src/day1.h
+++ src/day1.h
@@ -1,17 +1,98 @@
 #include "AocUtils.h"
 
 #include "gol/FileSystem.h"
+#include "gol/Vector.h"
+#include "gol/Algorithms.h"
+#include "timer.h"
 #include <stdio.h>
+
+#define PART 2
+
+int CharBuffToInt(gol::Slice<char> buff, int& idx)
+{
+	int result = 0;
+	for (; idx < buff.GetCount(); idx++)
+	{
+		const char c = buff[idx];
+		if (!AoC::CharIsNumber(c))
+			break;
+
+		result *= 10;
+		result += CHAR_TO_INT(c);;
+	}
+
+	return result;
+}
 
 void day1()
 {
-	gol::File file = AoC::LoadData(1, true);
+	gol::File file = AoC::LoadData(1);
 
 	char buff[32];
-	if (file.ReadTextLine(buff, 32))
-		printf("%s\n", buff);
-	else
-		printf("file failed to load :(\n");
+	file.ReadTextLine(buff, 32);
+	const size_t line_count = gol::StrLen(buff);
 
+	gol::Vector<int> column_a(line_count+1);
+	gol::Vector<int> column_b(line_count+1);
+
+	do 
+	{
+		int idx = 0;
+		int loc_id_a = CharBuffToInt(buff, idx);
+
+		while (!AoC::CharIsNumber(buff[idx]))
+			idx++;
+		
+		int loc_id_b = CharBuffToInt(buff, idx);
+
+		column_a.Push(loc_id_a);
+		column_b.Push(loc_id_b);
+	}
+	while (file.ReadTextLine(buff, 32));
 	file.Close();
+	
+	int total = 0;
+	int similarity_score = 0;
+	{
+		gol::Timer timer;
+		gol::Sort<int>(column_a);
+		gol::Sort<int>(column_b);
+
+#if PART == 1
+		for (int i = 0; i < column_a.GetCount(); i++)
+		{
+			total += gol::SubtractSmallestFromLargest(column_a[i], column_b[i]);
+		}
+#elif PART == 2
+		int last_recorded = -1;
+		int last_idx = 0;
+		int occurences = 0;
+	
+		//gol::Timer timer;
+		for (int i = 0; i < column_a.GetCount(); i++)
+		{
+			int a = column_a[i];
+			if (a == last_recorded)
+				similarity_score += a * occurences;
+			occurences = 0;
+			for (int j = last_idx; j < column_b.GetCount(); j++)
+			{
+				if (a != column_b[j])
+					continue;
+				if (column_b[j] > a)
+					last_idx = j;
+				occurences++;
+			}
+			last_recorded = a;
+			similarity_score += a * occurences;
+		}
+	}
+	
+#endif
+
+#if PART == 1
+	printf("total = %i\n", total);
+#elif PART == 2
+	printf("similarity_score = %i\n", similarity_score);
+#endif
 }
